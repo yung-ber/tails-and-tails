c6b3e21cd08a7dd539e681da8b1bc1bfccaea3a2
yung-ber software studios <57375758+yung-ber@users.noreply.github.com>
2020-09-19 17:52:44 -0400

    updated credits

--- CREDITS.cs
+++ CREDITS.cs
@@ -1,6 +1,5 @@
 ﻿// THIS PROJECT IS NOT RELATED TO OR LICENSED BY SEGA INC. OR ANY
 // OTHER LICENSE HOLDER OF SONIC THE HEDGEHOG
 // developed by not_sega
-// green hill zone by Nicky Roth
 // also, let's remember how long that not_sega spent applying mesh colliders to every part that needed it.
-//
+// if you have contributed to this project, feel free to add your name in the credits
